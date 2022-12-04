c389109a895aa57d235e448f80d30e1c52881225
nguyentamthanh <thanhh1602k@gmail.com>
2022-12-04 23:36:55 +0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="bg-blue-500 h-screen">
+    layout
+    <slot />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,4 @@
+<template>
+  <div class="bg-red-500">hihiiih</div>
+</template>
+<script setup></script>
